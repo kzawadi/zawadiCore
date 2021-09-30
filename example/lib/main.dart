f61075ac604280c886e00bf3f3c8e1308b325929
kelvin <kelvin.vitone@gmail.com>
2021-09-30 15:44:13 +0300

    iphone x pure test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
 
   runApp(
     DevicePreview(
-      enabled: true, //todo remember to switch this back to [!releaseMode]
+      enabled: false, //todo remember to switch this back to [!releaseMode]
       builder: (context) => MyApp(),
     ),
   );
